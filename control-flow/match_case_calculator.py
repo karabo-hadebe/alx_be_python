81eec8571a616fad79baefc2a1dcceba8836bd71
Karabo H <carchelivia@gmail.com>
2025-07-17 07:34:20 +0200

    Add match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,13 +3,13 @@ num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
-    case "+": print(num1+num2)
-    case "-": print(num1-num2)
-    case "*": print(num1*num2)
+    case "+": print(f"The result is {num1+num2}.")
+    case "-": print(f"The result is {num1-num2}.")
+    case "*": print(f"The result is {num1*num2}.")
     case "/": 
         if num2 != 0: 
-            print(num1/num2)
-        else: print("Error")
+            print(f"The result is {num1/num2}.")
+        else: print("Error! Cannot divide by zero.")
 
 
 
