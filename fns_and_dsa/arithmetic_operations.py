76b26d0504fa4d666375ccec6354147cc9892627
Karabo H <carchelivia@gmail.com>
2025-07-25 13:57:16 +0200

    Add arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    """Perform basic arithmetic operation based on the operation parameter."""
+
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Unknown operation"
